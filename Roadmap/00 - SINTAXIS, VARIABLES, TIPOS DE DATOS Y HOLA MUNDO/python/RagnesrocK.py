df267a22a60d1ac1919a40b45b7ff9d51ec9d911
RagnesrocK <117693740+RagnesrocK@users.noreply.github.com>
2024-05-01 16:00:09 -0600

    Create RagnesrocK.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/RagnesrocK.py
@@ -0,0 +1,27 @@
+#https://www.python.org/
+
+#Comentarp en una linea
+
+'''
+Esto es un ejemplo 
+de comentario
+'''
+
+my_variable = 'Mi variable'
+
+#Python no tiene constantes
+''' Pero se puede simular como variable '''
+
+MY_CONSTANT = "Mi constante" # no cambien el valor
+
+#Numero entero
+my_int = 5
+#Numero flotante
+my_float = 5.1
+#Boleanos
+my_boolt = True
+my_boolf = False
+#String
+my_str = "Mi texto"
+
+print("¡Hola, Python")
